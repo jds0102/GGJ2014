4ac257efb7bd62dffe1f36bb777a3932259b6c42
Justin Smith <jds0102@gmail.com>
2014-01-25 07:48:46 -0800

    Added function in Game Manager to pause the world while levels switch.

--- ProjectManwich/Assets/Scripts/GameManager.cs
+++ ProjectManwich/Assets/Scripts/GameManager.cs
@@ -7,13 +7,35 @@ public class GameManager : MonoBehaviour {
 
 	public GameObject player;
 
+	private GameObject m_enemyParent;
+
 	// Use this for initialization
 	void Start () {
 		m_singleton = this;
+		m_enemyParent = GameObject.Find ("Enemies");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	/// <summary>
+	/// This is where we need to stop everything while we switch levels
+	/// </summary>
+	public void PauseWorld() {
+		player.GetComponent<Rigidbody2D>().isKinematic = true;
+		foreach(Rigidbody2D enemy in m_enemyParent.GetComponentsInChildren<Rigidbody2D>()) {
+			enemy.isKinematic = true;
+			enemy.gameObject.GetComponent<Enemy>().moveSpeed = 0;
+		}
+	}
+
+	public void UnPauseWorld() {
+		player.GetComponent<Rigidbody2D>().isKinematic = false;
+		foreach(Rigidbody2D enemy in m_enemyParent.GetComponentsInChildren<Rigidbody2D>()) {
+			enemy.isKinematic = false;
+			enemy.gameObject.GetComponent<Enemy>().moveSpeed = 6;
+		}
 	}
 }
