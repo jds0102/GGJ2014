bb0bf1e0dc513448c391ebe088d4bb19ca59cd8c
DrDiabetes <spempe7@gmail.com>
2014-01-24 22:45:21 -0600

    asdf

--- ProjectManwich/Assets/Character.cs
+++ ProjectManwich/Assets/Character.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Character : MonoBehaviour
+{
+	private bool m_Jump = false;
 	
+	public float MoveForce = 365f;
+	public float MaxSpeed = 5f;
+	public float JumpForce = 1000f;	
+	
+	public CircleCollider2D GroundCollider;
+	
+	private Transform m_GroundCheck;			
+	private bool m_Grounded = false;
+	
+	
+	void Awake()
+	{
+		m_GroundCheck = transform.Find("groundCheck");
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Update()
+	{
+		m_Grounded = Physics2D.Linecast(transform.position, m_GroundCheck.position, 1 << LayerMask.NameToLayer("Ground") | 1 << LayerMask.NameToLayer("Platform")); 
+		
+		if(m_Grounded){
+			
+		}
+		
+		if(Input.GetButtonDown("Jump") && m_Grounded){
+			m_Jump = true;
+		}
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		float h = Input.GetAxis("Horizontal");
+		
+		if(h * rigidbody2D.velocity.x < MaxSpeed){
+			rigidbody2D.AddForce(Vector2.right * h * MoveForce);
+		}
+		
+		if(Mathf.Abs(rigidbody2D.velocity.x) > MaxSpeed){
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * MaxSpeed, rigidbody2D.velocity.y);
+		}
+
+		if(m_Jump)
+		{			
+			rigidbody2D.AddForce(Vector2.up * JumpForce);
+			
+			m_Jump = false;
+		}
+	}
+	
+	void UseAbility()
+	{
+		
 	}
 }
