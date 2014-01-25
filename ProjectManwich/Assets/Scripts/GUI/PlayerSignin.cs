9ccf0934485f6a921ad6a3dd623512c05bde7c90
John Karczewski <john@learninggamesnetwork.org>
2014-01-25 00:35:34 -0600

    blarg

---
+++ ProjectManwich/Assets/Scripts/GUI/PlayerSignin.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerSignin : MonoBehaviour {
+
+
+    private int m_prevSelectedIndex = -1;
+    private int m_selectedIndex = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+
+    }
+
+    private void HandleSelection()
+    {
+
+    }
+}
