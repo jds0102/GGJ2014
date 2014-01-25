3bdfd71ba9d9c964aee0fd9578a60286460c2430
Emanuel Rosu <rosu.emanuel@gmail.com>
2014-01-24 22:40:42 -0600

    added skill system and generic coroutine handler so coroutines can be managed with more precision than allowed by default

---
+++ ProjectManwich/Assets/Scripts/Skills/Stabilizer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//temporarily freezes heart rate to current
+public class Stabilizer : TimedSkill {
+/*	
+	public override void Execute() 
+	{
+		if (Locked || Cooldown) return;
+		if (!Activated) {
+			Activated = true;
+			PlayerCharacter.IgnoreBeatCycle = true;
+			StartTimer();
+			Locked = true;
+		}
+	}
+
+	public override void Finish() 
+	{
+		if (Locked) return;
+		Activated = false;
+		PlayerCharacter.IgnoreBeatCycle = false;
+	}*/
+}
