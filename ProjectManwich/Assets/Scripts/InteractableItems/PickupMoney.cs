c826c19f7784a577c27c51205687adfe7149f2f6
Justin Smith <jds0102@gmail.com>
2014-01-25 14:07:03 -0800

    More interactable item stuff

---
+++ ProjectManwich/Assets/Scripts/InteractableItems/PickupMoney.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupMoney : InteractableItem {
+
+	public int moneyValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	override public void Activate(Player player) {
+		m_activated = true;
+		GetComponent<SpriteRenderer> ().sprite = activatedImage;
+		player.m_money += moneyValue;
+	}
+
+	override public void Reset() {
+		m_activated = false;
+		GetComponent<SpriteRenderer> ().sprite = originalImage;
+	}
+}
