efa8741212b5073a83f55e122ac28c7c35e3b4ba
DrDiabetes <spempe7@gmail.com>
2014-01-25 22:17:29 -0600

    adfgasdf

--- ProjectManwich/Assets/Character.cs
+++ ProjectManwich/Assets/Character.cs
@@ -111,6 +111,7 @@ public class Character : MonoBehaviour
 
     public void FireSkill(int slot)
     {
+		Debug.Log (slot);
         Skill skillToFire = m_instancedSkills[slot];
         skillToFire.Execute();
 
