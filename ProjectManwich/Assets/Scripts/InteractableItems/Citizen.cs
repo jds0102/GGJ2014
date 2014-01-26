905d0c1800b9add37f22813ad9bcf59e23376d80
Justin Smith <jds0102@gmail.com>
2014-01-25 23:32:42 -0800

    Beginning of a civilian

---
+++ ProjectManwich/Assets/Scripts/InteractableItems/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Citizen : BreakableObject {
+
+	public GameObject m_moneyDrop;
+	public int m_value;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Called by players melee attack
+	override public void Activate(Player player) {
+		base.Activate(player);
+		GameObject money = (GameObject)Instantiate (m_moneyDrop);
+		money.GetComponent<MoneyDrop> ().Initiate (m_value, 2);
+		money.transform.position = transform.position;
+	}
+}
