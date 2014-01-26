dcb07ea3c51dcb4b5a6ab9567d0fa078b8f400c0
DrDiabetes <spempe7@gmail.com>
2014-01-25 23:15:04 -0600

    you're a mustache

---
+++ ProjectManwich/Assets/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+	public AudioSource[] AudioSources;
+	public List<AudioClip> Music = new List<AudioClip>();
+	public AudioClip[] SoundFX;
+
+	void Start()
+	{
+		OnLevelLoaded(-1);
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	void Update()
+	{
+	
+	}
+
+	void OnLevelLoaded(int id)
+	{
+		if(Application.loadedLevelName == "MainMenu"){
+			PlayMusic("MainTheme");
+		}
+	}
+
+	void PlayMusic(string music)
+	{
+		foreach(AudioClip song in Music){
+			if(song.name == music){
+				AudioSources[0].clip = song;
+				AudioSources[0].loop = true;
+				AudioSources[0].Play();
+			}
+		}
+
+	}
+}
